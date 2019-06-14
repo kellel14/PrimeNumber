63949c163bf48a6e34119926e0d100b5ce365d6d
kellel14 <kellel14@gmail.com>
2019-06-14 15:00:28 -0400

    2nd push: main class breaks with large #'s??  use main2 class

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int num;
-        String prime;
-        String cont;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a number to check if it is a prime number");
+        int num= keyboard.nextInt();
+//        String prime;
+//        String cont;
         boolean ifPrime = true;
 
 
-        Scanner keyboard = new Scanner(System.in);
-        num = keyboard.nextInt();
 
 
         //printing all numbers between 0 and 10,000
@@ -21,24 +21,22 @@ public class Main {
         //prime numbers factors are only 1 and itself...number == divide by 2 -> prime number
         for (int p = 2; p <= Math.sqrt(num); p++) {
 
-            System.out.println("Don't know your prime numbers? Check here!");
-            num = keyboard.nextInt();
-
             if (num % p == 0) {
                 keyboard.nextInt();
                 ifPrime = false;
-                System.out.println(num + " Is not a prime number");
+//                System.out.println(num + " Is not a prime number");
             }
-            if (ifPrime) {
-                System.out.println(num + " Is a prime number");
-            }
-
-
         }
 
+        if (ifPrime == false) {
+            System.out.println(num + " " + "is not a prime number");
+        }
 
-
+        else if (ifPrime == true) {
+            System.out.println(num + " " + "is a prime number");
+        }
     }
+
 
 //
 //        System.out.println("Would like to see a list of prime numbers or generate a number?");
--- src/Main2.java
+++ src/Main2.java
@@ -4,45 +4,43 @@ public class Main2 {
 
     public static void main(String[] args) {
 
-        int num;
-//        String prime;
-//        String cont;
-        boolean ifPrime = true;
-        int maxNum= 10000;
 
-
+        System.out.println("Don't know your prime numbers? Check here!");
         Scanner keyboard = new Scanner(System.in);
-//        num = keyboard.nextInt();
+        int num = keyboard.nextInt();
 
+//        String prime;
+        String cont;
+        boolean ifPrime = true;
+//        int maxNum= 10000;
+
+
+//        num = keyboard.nextInt();
 
         //printing all numbers between 0 and 10,000
         //initialize that we increment by 1
         //this function will be for user input
         //prime numbers are greater than 1
         //prime numbers factors are only 1 and itself...number == divide by 2 -> prime number
-        System.out.println("Don't know your prime numbers? Check here!");
-        for (int p = 1; p <=  maxNum; p++) {
+
+        for (int p = 2; p <= Math.sqrt(num); p++) {
 //            for (int p = 2; maxNum <= p; num++){
 
-
-                num = keyboard.nextInt();
-
-                if (num % p == 0) {
-//                keyboard.nextInt();
-                    ifPrime = false;
-                    System.out.println(num + " Is not a prime number");
-                    }
-
-
-                }
-                if (ifPrime) {
-                    System.out.println(num + " Is a prime number");
-                }
-//            }
-
+            if (num % p == 0) {
+                ifPrime = false;
+                System.out.println(num + " is not a prime number");
+            }
 
         }
+        if (ifPrime) {
+            System.out.println(num + " is a prime number");
 
-
+        }
     }
+
+//            System.out.println("Do you want to enter another number (Y/N)?");
+//            cont = keyboard.next();
+//
+//        }while (cont.equalsIgnoreCase("Y")) ;
+//    }
 }
